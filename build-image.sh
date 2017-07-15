8602b2926679df176b92fcf7629bdc5cf7a923c4
Alistair Lynn <arplynn@gmail.com>
2017-07-15 16:16:07 +0100

    apt stages

--- build-image.sh
+++ build-image.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 IMAGE=https://downloads.raspberrypi.org/raspbian_latest
 
+echo "Updating apt"
+sudo apt-get update
+
+echo "Installing dependencies"
+sudo apt-get install curl
+
 set -e
 echo "Downloading Raspbian image"
 curl -L $IMAGE -o raspbian.zip
