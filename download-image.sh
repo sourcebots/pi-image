afec6deda55a9f1f63de195b7c6a35e91e83db4d
Alistair Lynn <arplynn@gmail.com>
2017-07-15 19:25:39 +0100

    Store the image in /tmp

--- download-image.sh
+++ download-image.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+
+TARGET=${1:-raspbian.zip}
 IMAGE=https://downloads.raspberrypi.org/raspbian_lite_latest
 
 set -e
 
-if [ -f raspbian.zip ]; then
+if [ -f $TARGET ]; then
     echo "Image already downloaded."
     exit 0
 fi
 
 echo "Downloading Raspbian image"
-curl -L $IMAGE -o raspbian.zip
+curl -L $IMAGE -o $TARGET
 
