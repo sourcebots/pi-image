c5896eb37f4a6b1fa25722ce88f06ea128794139
Alistair Lynn <arplynn@gmail.com>
2017-07-15 19:53:20 +0100

    Bypass ld preload

--- build-image.sh
+++ build-image.sh
@@ -14,11 +14,14 @@ mount /dev/mapper/loop0p2 $TARGET
 
 cp /usr/bin/qemu-arm-static $TARGET/usr/bin/qemu-arm-static
 cp main.sh $TARGET/main.sh
+# Disable ld preload
+mv $TARGET/ld.so.preload ./ld.so.preload
 
 chroot $TARGET /main.sh
 
 rm $TARGET/main.sh
 rm $TARGET/usr/bin/qemu-arm-static
+mv ./ld.so.preload $TARGET/ld.so.preload
 
 umount $TARGET
 sync
