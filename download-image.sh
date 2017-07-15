102ca72299a475537eff5d5ab8915a4fa2681e43
Alistair Lynn <arplynn@gmail.com>
2017-07-15 16:28:53 +0100

    Only download the image

--- download-image.sh
+++ download-image.sh
@@ -2,6 +2,12 @@
 IMAGE=https://downloads.raspberrypi.org/raspbian_latest
 
 set -e
+
+if [ -f raspbian.zip ]; then
+    echo "Image already downloaded."
+    exit 0
+fi
+
 echo "Downloading Raspbian image"
 curl -L $IMAGE -o raspbian.zip
 unzip raspbian.zip
