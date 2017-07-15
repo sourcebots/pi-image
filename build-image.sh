21eee73368b27349c2e604a93036a839af0fa2c2
Alistair Lynn <arplynn@gmail.com>
2017-07-15 16:13:41 +0100

    Follow redirects

--- build-image.sh
+++ build-image.sh
@@ -3,7 +3,7 @@ IMAGE=https://downloads.raspberrypi.org/raspbian_latest
 
 set -e
 echo "Downloading Raspbian image"
-curl $IMAGE -o raspbian.zip
+curl -L $IMAGE -o raspbian.zip
 unzip raspbian.zip
 ls *
 
