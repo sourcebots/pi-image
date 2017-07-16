27caba374e1f597722277fa5396214a58512d57d
Alistair Lynn <arplynn@gmail.com>
2017-07-16 15:29:39 +0100

    Upgrade to Debian Stretch

--- pi-prebuild.sh
+++ pi-prebuild.sh
@@ -10,6 +10,13 @@ apt-get update -y
 
 echo "Upgrading anything out of date"
 apt-get upgrade -y
+apt-get dist-upgrade -y
+
+echo "Upgrade to Debian Stretch"
+sed -i 's/jessie/stretch/g' /etc/apt/sources.list
+apt-get update -y
+apt-get upgrade -y
+apt-get dist-upgrade -y
 
 echo "Installing git"
 apt-get install -y git
