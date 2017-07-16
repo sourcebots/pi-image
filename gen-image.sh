b772ff9125bcdce9262bb8fa7ec2e42c80eeb131
Alistair Lynn <arplynn@gmail.com>
2017-07-16 19:24:32 +0100

    Do this earlier

--- gen-image.sh
+++ gen-image.sh
@@ -4,6 +4,6 @@ echo 'deb [arch=armhf] http://ports.ubuntu.com/ trusty main restricted' >> /etc/
 echo 'deb-src [arch=armhf] http://ports.ubuntu.com/ trusty main restricted' >> /etc/apt/sources.list
 cat /etc/apt/sources.list
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys A4A9406876FCBD3C456770C88C718D3B5072E1F5
+sudo dpkg --add-architecture armhf
 sudo apt-get update -y
 sudo apt-get install -y crossbuild-essential-armhf
-sudo dpkg --add-architecture armhf
