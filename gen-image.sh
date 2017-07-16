27f6ec1dc1cbb35d11ab2055460cf097d085527d
Alistair Lynn <arplynn@gmail.com>
2017-07-16 19:11:41 +0100

    Grab this key too

--- gen-image.sh
+++ gen-image.sh
@@ -3,5 +3,6 @@ set -e
 echo 'deb [arch=armhf] http://us.archive.ubuntu.com/ubuntu/ trusty main restricted' >> /etc/apt/sources.list
 echo 'deb-src [arch=armhf] http://us.archive.ubuntu.com/ubuntu/ trusty main restricted' >> /etc/apt/sources.list
 cat /etc/apt/sources.list
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys A4A9406876FCBD3C456770C88C718D3B5072E1F5
 sudo apt-get update -y
 sudo apt-get install -y crossbuild-essential-armhf
