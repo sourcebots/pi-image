96b95053488a95d38c81845805e4d7ad1b75b13e
Alistair Lynn <arplynn@gmail.com>
2017-07-16 19:12:50 +0100

    Do the rest of the ARM incantation

--- gen-image.sh
+++ gen-image.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
-echo 'deb [arch=armhf] http://us.archive.ubuntu.com/ubuntu/ trusty main restricted' >> /etc/apt/sources.list
-echo 'deb-src [arch=armhf] http://us.archive.ubuntu.com/ubuntu/ trusty main restricted' >> /etc/apt/sources.list
+echo 'deb [arch=armhf] https://ports.ubuntu.com/ trusty main restricted' >> /etc/apt/sources.list
+echo 'deb-src [arch=armhf] http://ports.ubuntu.com/ trusty main restricted' >> /etc/apt/sources.list
 cat /etc/apt/sources.list
+sudo dpkg --add-architecture armhf
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys A4A9406876FCBD3C456770C88C718D3B5072E1F5
 sudo apt-get update -y
 sudo apt-get install -y crossbuild-essential-armhf
