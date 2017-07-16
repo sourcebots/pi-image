7985f618adfb8530ad59e5ee25650f587c94c6f3
Alistair Lynn <arplynn@gmail.com>
2017-07-16 19:04:41 +0100

    Do some more things

--- gen-image.sh
+++ gen-image.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo 'deb [arch=armhf] http://us-east1.gce.archive.ubuntu.com/ubuntu/ trusty main restricted' >> /etc/apt/sources.list
 cat /etc/apt/sources.list
+sudo apt-get update -y
 sudo apt-get install -y crossbuild-essential-armhf
