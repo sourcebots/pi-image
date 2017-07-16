0075c5a1b5bb6d0fe896e485f6d2e268aa7bf050
Alistair Lynn <arplynn@gmail.com>
2017-07-16 19:08:36 +0100

    set -e

--- gen-image.sh
+++ gen-image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 echo 'deb [arch=armhf] http://us.archive.ubuntu.com/ubuntu/ trusty main restricted' >> /etc/apt/sources.list
 echo 'deb-src [arch=armhf] http://us.archive.ubuntu.com/ubuntu/ trusty main restricted' >> /etc/apt/sources.list
 cat /etc/apt/sources.list
