ba370bd507c4047a199d92c2fe8483657e994f14
Alistair Lynn <arplynn@gmail.com>
2017-07-23 20:07:22 +0100

    Grab from a different keyserver

--- pi-prebuild.sh
+++ pi-prebuild.sh
@@ -11,7 +11,7 @@ export APT_LISTCHANGES_FRONTEND=none
 echo "Adding backports"
 echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
 mkdir -p ~/.gnupg
-gpg --keyserver subkeys.pgp.net --recv-keys 7638D0442B90D010
+gpg --keyserver keyring.debian.org --recv-keys 7638D0442B90D010
 gpg -a --export 7638D0442B90D010 | apt-key add -
 
 echo "Updating apt"
