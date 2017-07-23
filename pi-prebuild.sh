98658a784f08d0c7b9fadb1806d51dcbcd8a7b11
Alistair Lynn <arplynn@gmail.com>
2017-07-23 20:02:02 +0100

    Do the thing

--- pi-prebuild.sh
+++ pi-prebuild.sh
@@ -10,6 +10,7 @@ export APT_LISTCHANGES_FRONTEND=none
 
 echo "Adding backports"
 echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
+mkdir -p ~/.gnupg
 gpg --keyserver subkeys.pgp.net --recv-keys 7638D0442B90D010
 gpg -a --export 7638D0442B90D010 | apt-key add -
 
