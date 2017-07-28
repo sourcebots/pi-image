e8ace58c5e7693fc7763ef5305da87337fa5d5d5
Alistair Lynn <arplynn@gmail.com>
2017-07-28 19:30:07 +0100

    Cache some useful components

--- pi-prebuild.sh
+++ pi-prebuild.sh
@@ -24,6 +24,14 @@ apt-get install -y git
 echo "Installing build tools"
 apt-get install -y build-essential devscripts debhelper dh-systemd
 
+echo "Installing some useful cached bits for downstream builds"
+apt-get install -y \
+    libopencv-core-dev \
+    libopencv-contrib-dev \
+    libopencv-video-dev \
+    libopencv-photo-dev \
+    python3-dev
+
 echo "Rebuilding locale"
 locale-gen en_US en_US.UTF-8
 locale-gen en_GB en_GB.UTF-8
