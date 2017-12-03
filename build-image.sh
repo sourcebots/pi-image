21f4a02c49a52db7b507e4fb76dbe48a6fe42e00
Jake Howard <git@theorangeone.net>
2017-12-03 21:24:44 +0000

    Explicitly install additional libraries

--- build-image.sh
+++ build-image.sh
@@ -26,6 +26,8 @@ mount --bind $PWD/components $TARGET/building
 echo "Copying in some drive scripts"
 cp /usr/bin/qemu-arm-static $TARGET/usr/bin/qemu-arm-static
 cp pi-main.sh $TARGET/main.sh
+cp additional-libraries.txt $TARGET/additional-libraries.txt
+
 # Disable ld preload
 mv $TARGET/etc/ld.so.preload ./ld.so.preload
 
