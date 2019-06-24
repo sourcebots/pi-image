9de069054774a7229250e601a90097849d8e5cf9
Dan Trickey <trickeydan@users.noreply.github.com>
2019-06-24 11:51:16 +0100

    Update base image to Raspbian Buster

--- download-image.sh
+++ download-image.sh
@@ -5,9 +5,9 @@
 TARGET=${1:-raspbian.zip}
 # Note: if updating the image we're using, you should also update the hash
 # below. The hashes are available from Raspberry Pi as "$IMAGE.sha256".
-IMAGE=https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2017-12-01/2017-11-29-raspbian-stretch-lite.zip
+IMAGE=https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2019-06-24/2019-06-20-raspbian-buster-lite.zip
 
-echo "e942b70072f2e83c446b9de6f202eb8f9692c06e7d92c343361340cc016e0c9f $TARGET" > sha256
+echo "9009409a9f969b117602d85d992d90563f181a904bc3812bdd880fc493185234 $TARGET" > sha256
 
 # If we already have the file, and it's got the right hash, then skip the download
 sha256sum --check sha256 && echo "Image already downloaded." && exit 0
